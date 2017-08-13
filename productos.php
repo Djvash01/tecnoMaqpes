dfff8baad837e487eda2a56d549c859f558b4aa9
alaverde <andreslaverde11@gmail.com>
2017-08-12 20:02:32 -0500

    productos.php

---
+++ productos.php
@@ -0,0 +1,227 @@
+<!DOCTYPE HTML>
+<!--
+	Dopetrope by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>TecnoMaqpes</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link href=images/favicon.png rel="shortcut icon" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="homepage">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<div id="header-wrapper">
+					<div id="header">
+
+						<!-- Logo -->
+							<h1><a href="index.html"><img src="images/logo2.png" alt="" /></a></h1>
+
+						<!-- Nav -->
+							<nav id="nav">
+								<ul>
+									<li><a href="index.html">INICIO</a></li>
+									<li><a href="quienessomos.html">QUIENES SOMOS</a></li>
+									<li class="current"><a href="#">PRODUCTOS</a>
+										<ul>
+											<li><a href="#">+ Hidraulicos</a>
+												<ul>
+													<li><a href="#">Bombas hidraulicas</a></li>
+													<li><a href="#">Motores de traslacion</a></li>
+													<li><a href="#">Rotores de traslacion</a></li>
+													<li><a href="#">Cilindros</a></li>
+													<li><a href="#">Mangueras</a></li>
+													<li><a href="#">Motores de giros</a></li>
+													<li><a href="#">Reductores de giros</a></li>
+												</ul>
+											</li>
+											<li><a href="#">Electronicos</a></li>
+											<li><a href="#">Mecanicos</a></li>
+											<li><a href="#">Aire acondicionado</a></li>
+										</ul>
+									</li>
+									<li><a href="servicios.html">SERVICIOS</a></li>
+									<li><a href="pedidos.html">PEDIDOS</a></li>
+								</ul>
+							</nav>
+						
+					</div>
+				</div>
+
+			<!-- Main -->
+				<div id="main-wrapper">
+
+					<div class="container">
+
+							<article class="box post">
+								<header>
+									<h3>
+										<?php
+											echo $_GET["categoria"];
+										?>
+									</h3>
+								</header>
+							</article>
+
+						<div class="row">
+							<div class="12u">
+
+								<!-- Portfolio -->
+									<section>
+										<div class="row">
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+													<header>
+														<h3>Ipsum feugiat et dolor</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Find out more</a>
+													</footer>
+												</section>
+											</div>
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic03.jpg" alt="" /></a>
+													<header>
+														<h3>Sed etiam lorem nulla</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Find out more</a>
+													</footer>
+												</section>
+											</div>
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic04.jpg" alt="" /></a>
+													<header>
+														<h3>Consequat et tempus</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Find out more</a>
+													</footer>
+												</section>
+											</div>
+										</div>
+										<div class="row">
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic05.jpg" alt="" /></a>
+													<header>
+														<h3>Blandit sed adipiscing</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Find out more</a>
+													</footer>
+												</section>
+											</div>
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic06.jpg" alt="" /></a>
+													<header>
+														<h3>Etiam nisl consequat</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Find out more</a>
+													</footer>
+												</section>
+											</div>
+											<div class="4u 12u(mobile)">
+												<section class="box">
+													<a href="#" class="image featured"><img src="images/pic07.jpg" alt="" /></a>
+													<header>
+														<h3>Dolore nisl feugiat</h3>
+													</header>
+													<p>Lorem ipsum dolor sit amet sit veroeros sed amet blandit consequat veroeros lorem blandit  adipiscing et feugiat phasellus tempus dolore ipsum lorem dolore.</p>
+													<footer>
+														<a href="#" class="button alt">Find out more</a>
+													</footer>
+												</section>
+											</div>
+										</div>
+									</section>
+
+							</div>
+						</div>
+					</div>
+				</div>
+
+			<!-- Footer -->
+				<div id="footer-wrapper">
+					<section id="footer" class="container">
+						<div class="row">
+							<div  class="12u 12u(mobile)">
+								<section>
+									<header>
+										<h2 id="titlefooter">Encuentranos en las redes sociales</h2>
+									</header>
+									<ul class="social">
+										<li><a class="icon fa-facebook" href="#"><span class="label">Facebook</span></a></li>
+										<li><a class="icon fa-twitter" href="#"><span class="label">Twitter</span></a></li>
+										<li><a class="icon fa-google-plus" href="#"><span class="label">Google+</span></a></li>
+									</ul>
+									<ul class="contact">
+										<li>
+											<h3>Contacto</h3>
+											<p>
+												James Castro<br />
+												Servicios Mecatronicos<br />
+												Movil: (+57) 314 641 66 56<br />
+												Correo: <a href="#">jaaldu@gmail.com</a>
+											</p>
+											<br/>
+											<p>
+												Natali Yesenia Marulanda<br />
+												Asesora Comercial<br />
+												Movil: (+57) 313 788 17 52<br />
+												Correo: <a href="#">nayemalo@hotmail.com</a>
+											</p>
+										</li>
+										<li>
+											<h3>Teléfono</h3>
+											<p>(+57) (034) 601 65 23</p>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+						<div class="row">
+							<div class="12u">
+
+								<!-- Copyright -->
+									<div id="copyright">
+										<ul class="links">
+											<li>&copy; Tecnomaqpes. Todos los derechos reservados.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+										</ul>
+									</div>
+
+							</div>
+						</div>
+					</section>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/skel-viewport.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
